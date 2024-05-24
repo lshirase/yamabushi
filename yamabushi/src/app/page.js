79abe9d15c441e0d206aac8f31ea61a7f1005d9b
lshirase <lshirase@umich.edu>
2024-05-23 15:20:19 -1000

    fonts n what not

--- yamabushi/src/app/page.js
+++ yamabushi/src/app/page.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import yamabushi from "../../public/yamabushi.svg";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div>Yamabushi Farms</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white font-bianzhidai text-xs text-black">
+      <div>YAMABUSHI FARMS</div>
+      <Image src={yamabushi} width="500" height="500" />
+      <div>
+        LOCALLY GROWN LION’S MANE <br />
+        LOS ANGELES, CA
+      </div>
+      <div className="flex flex-row">
+        <Link href="instagram.com/yamabushifarms">INSTAGRAM</Link>
+        <Link href="instagram.com/yamabushifarms">EMAIL</Link>
+      </div>
     </main>
   );
 }
